ffbb054b7ea81ae2d764e5ab72bd233bd80a86d0
[oluwafemi olawale] <olawalefemi041@gmail.com>
2025-01-21 07:52:13 +0100

    Sunny-side

--- sunny-side-landing-page/src/component/Section.jsx
+++ sunny-side-landing-page/src/component/Section.jsx
@@ -1,11 +1,9 @@
+const Section = () => {
+  return (
+    <div className="section">
+      <h1>Client Testimonies</h1>
+    </div>
+  );
+};
 
-
-const Section = ()=>{
-   return(
-       <div className="section">
-        <h1>Client Testimonies</h1>
-       </div>
-   );
-}
-
-export default Section
+export default Section;
